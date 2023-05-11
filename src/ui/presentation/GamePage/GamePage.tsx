96975e775d96b5acede846b102eade678ffa60cc
Brian Vu <84050828+bvu12@users.noreply.github.com>
2023-05-11 15:40:52 -0700

    refactor: use context to clean-up some components

--- src/ui/presentation/GamePage/GamePage.tsx
+++ src/ui/presentation/GamePage/GamePage.tsx
@@ -2,17 +2,55 @@ import GamePageCardGroup, { GamePageCardGroupProps } from "./GamePageCardGroup";
 import GamePageTransitionCheck, {
   GamePageTransitionCheckProps,
 } from "./GamePageTransitionCheck";
+import { useContext, useState } from "react";
 
-export type GamePageProps = GamePageCardGroupProps &
-  GamePageTransitionCheckProps;
+import { GameStateContext } from "../SsenseHigherLowerGame/SsenseHigherLowerGame";
+import { Product } from "@/interfaces";
+import { useTimeoutFn } from "react-use";
 
-const GamePage = ({
-  givenProduct,
-  unknownProduct,
-  onGuessHigher,
-  onGuessLower,
-  isShowingTransition,
-}: GamePageProps) => {
+export type GamePageProps = {
+  products: Product[];
+};
+
+const GamePage = ({ products }: GamePageProps) => {
+  const { productIndex, setProductIndex } = useContext(GameStateContext);
+  const givenProduct = products[productIndex];
+  const unknownProduct = products[productIndex + 1];
+
+  const [isShowingTransition, setIsShowingTransition] = useState(false);
+  let [, , resetIsShowingTransition] = useTimeoutFn(
+    () => setIsShowingTransition(false),
+    1000
+  );
+
+  function guessHandler(comparator: {
+    (a: number, b: number): boolean;
+    (arg0: number, arg1: number): any;
+  }) {
+    if (
+      comparator(
+        unknownProduct.priceByCountry[0].regular,
+        givenProduct.priceByCountry[0].regular
+      )
+    ) {
+      setIsShowingTransition((isShowingTransition) => !isShowingTransition);
+      resetIsShowingTransition();
+      setProductIndex((previousProductIndex) => previousProductIndex + 1);
+    } else {
+      // setIsGameOver(true);
+    }
+  }
+
+  function onGuessHigher() {
+    // Guess that the unknown product is higher in price than the given product
+    guessHandler((a: number, b: number) => a >= b);
+  }
+
+  function onGuessLower() {
+    // Guess that the unknown product is lower in price than the given product
+    guessHandler((a: number, b: number) => a <= b);
+  }
+
   return (
     <div className="relative">
       <GamePageCardGroup
--- src/ui/presentation/SsenseHigherLowerGame/SsenseHigherLowerGame.tsx
+++ src/ui/presentation/SsenseHigherLowerGame/SsenseHigherLowerGame.tsx
@@ -1,68 +1,37 @@
+import React, { createContext } from "react";
+
 import GameOverPage from "../GameOverPage/GameOverPage";
 import GamePage from "../GamePage/GamePage";
 import { Product } from "@/interfaces";
 import { useState } from "react";
-import { useTimeoutFn } from "react-use";
+
+const useGameValues = () => {
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [productIndex, setProductIndex] = useState(0);
+
+  return {
+    isGameOver,
+    setIsGameOver,
+    productIndex,
+    setProductIndex,
+  };
+};
+
+export const GameStateContext = createContext(
+  {} as ReturnType<typeof useGameValues>
+);
 
 type SsenseHigherLowerGameProps = {
   products: Product[];
 };
 
 const SsenseHigherLowerGame = ({ products }: SsenseHigherLowerGameProps) => {
-  const [isGameOver, setIsGameOver] = useState(false);
-  const [productIndex, setProductIndex] = useState(0);
-
-  const [isShowingTransition, setIsShowingTransition] = useState(false);
-  let [, , resetIsShowingTransition] = useTimeoutFn(
-    () => setIsShowingTransition(false),
-    1000
-  );
-
-  function guessHandler(comparator: {
-    (a: number, b: number): boolean;
-    (arg0: number, arg1: number): any;
-  }) {
-    const givenProduct = products[productIndex];
-    const unknownProduct = products[productIndex + 1];
-
-    if (
-      comparator(
-        unknownProduct.priceByCountry[0].regular,
-        givenProduct.priceByCountry[0].regular
-      )
-    ) {
-      setIsShowingTransition((isShowingTransition) => !isShowingTransition);
-      resetIsShowingTransition();
-      setProductIndex((previousProductIndex) => previousProductIndex + 1);
-    } else {
-      // setIsGameOver(true);
-    }
-  }
-
-  function onGuessHigher() {
-    // Guess that the unknown product is higher in price than the given product
-    guessHandler((a: number, b: number) => a >= b);
-  }
-
-  function onGuessLower() {
-    // Guess that the unknown product is lower in price than the given product
-    guessHandler((a: number, b: number) => a <= b);
-  }
+  const { isGameOver } = useGameValues();
 
   return (
-    <>
-      {isGameOver ? (
-        <GameOverPage />
-      ) : (
-        <GamePage
-          givenProduct={products[productIndex]}
-          unknownProduct={products[productIndex + 1]}
-          onGuessHigher={onGuessHigher}
-          onGuessLower={onGuessLower}
-          isShowingTransition={isShowingTransition}
-        />
-      )}
-    </>
+    <GameStateContext.Provider value={useGameValues()}>
+      {isGameOver ? <GameOverPage /> : <GamePage products={products} />}
+    </GameStateContext.Provider>
   );
 };
 
