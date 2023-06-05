7ce336c43755be5ceca12023a2f27e0e5748567c
bvu12 <brian.vu12@gmail.com>
2023-06-04 21:10:17 -0700

    refactor: run prettier

--- src/context/useGameStateContext.ts
+++ src/context/useGameStateContext.ts
@@ -1,29 +1,27 @@
+import { MongoProduct } from "@/interfaces";
 import { createContext, useState } from "react";
 
-import { MongoProduct } from "@/interfaces";
-
 export const useGameState = () => {
-    // Game state
-    const [isLoading, setIsLoading] = useState<boolean>(true);
-    const [isGameOver, setIsGameOver] = useState<boolean>(false);
-  
-    // Products
-    const [products, setProducts] = useState<MongoProduct[]>([]);
-    const [productIndex, setProductIndex] = useState<number>(0);
-  
-    return {
-      isLoading,
-      setIsLoading,
-      isGameOver,
-      setIsGameOver,
-      products,
-      setProducts,
-      productIndex,
-      setProductIndex,
-    };
+  // Game state
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isGameOver, setIsGameOver] = useState<boolean>(false);
+
+  // Products
+  const [products, setProducts] = useState<MongoProduct[]>([]);
+  const [productIndex, setProductIndex] = useState<number>(0);
+
+  return {
+    isLoading,
+    setIsLoading,
+    isGameOver,
+    setIsGameOver,
+    products,
+    setProducts,
+    productIndex,
+    setProductIndex,
   };
-  
-  export const GameStateContext = createContext(
-    {} as ReturnType<typeof useGameState>
-  );
-  
+};
+
+export const GameStateContext = createContext(
+  {} as ReturnType<typeof useGameState>
+);
