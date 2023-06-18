d7b520b1211f089039695beb5a16bf5dcd8b1152
bvu12 <brian.vu12@gmail.com>
2023-06-17 23:05:49 -0700

    chore: add production env URL

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,10 @@ import { Spinner } from "@/ui/basic/Spinner/Spinner";
 import SsenseHigherLowerGame from "@/ui/presentation/SsenseHigherLowerGame/SsenseHigherLowerGame";
 import { useEffect, useState } from "react";
 
+const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  : "http://localhost:3000";
+
 export default function Home() {
   const {
     isGameOver,
@@ -21,9 +25,7 @@ export default function Home() {
 
   const fetchProducts = async (productAPISuffix: string) => {
     setIsLoading(true);
-    const res = await fetch(
-      "http://localhost:3000/api/products/" + productAPISuffix
-    );
+    const res = await fetch(`${baseUrl}/api/products/${productAPISuffix}`);
     const data = await res.json();
     setProductIndex(0);
     setProducts(data);
@@ -34,7 +36,7 @@ export default function Home() {
   const fetchMoreProducts = () => {
     // As the game progresses, keep fetching more products on an interval in the background
     if (productIndex > 0 && productIndex % 5 == 0) {
-      fetch("http://localhost:3000/api/products/" + gameType)
+      fetch(`${baseUrl}/api/products/${gameType}`)
         .then((res) => res.json())
         .then((data) => setProducts((products) => [...products, ...data]));
     }
